fc437138630cd0d05d63a6d1d98d763b470a804e
Vina27 <Mtabali27@gmail.com>
2020-08-28 18:19:56 -0400

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@ Restaurant.destroy_all
 Review.destroy_all 
 
 may = User.create(user_name:"may", password:"may")
-ssgrill = Restaurant.create(name: "Shobu Sushi & Grill", street_address: "9427 5th Ave", city: "Brooklyn", state: "NY", zip_code:11209, category: "Japanese, Sushi Bar")
-Review.create(restaurant_id: ssgrill.id, user_id: may.id, content: "Delicous sushi rolls", title: "Excellent Meal")
+
+#ssgrill = Restaurant.create(name: "Shobu Sushi & Grill", street_address: "9427 5th Ave", city: "Brooklyn", state: "NY", zip_code:11209, category: "Japanese, Sushi Bar")
+#Review.create(user_id: may.id, restaurant_id: ssgrill.id, content: "Delicious sushi rolls", title: "Excellent Meal")
 
 ssgrill = Restaurant.create(name: "Shobu Sushi & Grill", street_address: "9427 5th Ave", city: "Brooklyn", state: "NY", zip_code:11209, category: "Japanese, Sushi Bar")
 chip = Restaurant.create(name: "Chipolte Mexican Grill", street_address: "463 86th St", city: "Brooklyn", state: "NY", zip_code:11209, category: "Mexican")
@@ -21,4 +22,9 @@ counter = Restaurant.create(name: "The Counter", street_address: "7 Times Square
 katz = Restaurant.create(name: "Katz's Delicatessen", street_address: "205 E Houston St", city: "New York", state: "NY", zip_code:10002, category: "Deli, Sandwiches")
 
 
-
+Review.create(user_id: may.id, restaurant_id: ssgrill.id, content: "The sushi is always fresh and delicious here!", title: "Excellent Food")
+Review.create(user_id: may.id, restaurant_id: chip.id, content: "Food is always fresh and tasty!", title: "My Go To")
+Review.create(user_id: may.id, restaurant_id: ichi.id, content: "Huge variety of fresh sushi!", title: "Best Sushi Buffet")
+Review.create(user_id: may.id, restaurant_id: lb.id, content: "Must try the Gramdma Pizza!", title: "Best Pizza")
+Review.create(user_id: may.id, restaurant_id: counter.id, content: "Create your own delicious burger!", title: "So Tasty")
+Review.create(user_id: may.id, restaurant_id: katz.id, content: "The pastrami is to die for!", title: "Iconic Deli")
