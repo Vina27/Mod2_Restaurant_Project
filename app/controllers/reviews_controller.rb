a8d1d5b28684d29e26eb3aca1d4a49c96cb4599d
Vina27 <Mtabali27@gmail.com>
2020-09-01 11:01:23 -0400

    review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+    def index
+        @reviews = Review.all 
+    end
+    
+    def new
+        # byebug
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @review = Review.new(restaurant:@restaurant)
+    end 
+
 end
