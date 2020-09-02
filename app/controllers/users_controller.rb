472a4150fa41ef38d90259c5c5de308532209a6d
Vina27 <Mtabali27@gmail.com>
2020-09-02 14:52:03 -0400

    Added User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
     def index
         @user= User.all
     end
+
+    def new 
+        @user = User.new
+    end 
+
+    def create 
+        # byebug
+        @user = User.create(user_params(:user_name, :password))
+        redirect_to restaurants_path
+    end 
+
+    private 
+    def user_params(*args) 
+        params.require(:user).permit(*args)
+    end 
+
+
 end
